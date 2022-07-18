0175f084c79300ada870fbeee62bf6393ddbc3a6
Tran Khac Thien <tran.khac.thien@sun-asterisk.com>
2022-07-18 09:23:45 +0000

    Create fund_credentials and funds

---
+++ app/Models/Fund.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Fund extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'historical_data_url',
+    ];
+
+    public function credential()
+    {
+        return $this->belongsTo(FundCredential::class);
+    }
+}
---
+++ database/migrations/2022_07_18_085403_create_fund_credentials_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fund_credentials', function (Blueprint $table) {
+            $table->string('id')->unique()->nullable(false);
+            $table->string('token')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fund_credentials');
+    }
+};
