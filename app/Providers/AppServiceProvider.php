4e8bf5cfc4b7e8ec0e771574b6e754bfc5c96269
Tran Khac Thien <tran.khac.thien@sun-asterisk.com>
2022-08-14 16:24:03 +0700

    Implemented reset password

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        ResetPassword::createUrlUsing(function ($notifiable, $token) {
+            return env('SPA_URL') . "/reset-password/{$token}?email={$notifiable->getEmailForPasswordReset()}";
+        });
     }
 }
