00875974776dcf964538be10f999bc01ac2ad42b
thienkt <thienkhac8@gmail.com>
2022-11-13 05:18:45 +0000

    fix

--- routes/channels.php
+++ routes/channels.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Log;
 */
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    Log::log("LOGGGGGGGGGGGGGGGGGGGGGGGGG:::::::::::::1");
     return (int) $user->id === (int) $id;
 });
 
 Broadcast::channel('notification.{userId}', function ($user, $userId) {
+    Log::log("LOGGGGGGGGGGGGGGGGGGGGGGGGG:::::::::::::2");
     return (int) $user->id === (int) $userId;
 });
